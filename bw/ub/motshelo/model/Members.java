d60bbe10bc58da932b7be898e88b7831df7e57e1
thapelokennedy131-droid <thapelokennedy131@gmail.com>
2026-05-01 16:36:52 +0200

    update

--- bw/ub/motshelo/model/Members.java
+++ bw/ub/motshelo/model/Members.java
@@ -1,13 +1,13 @@
 package bw.ub.motshelo.model;
 import java.util.ArrayList;
 
-public class Member {
+public class Members {
     private String name;
     private String omang;
     private double balance;
     private ArrayList<Transaction> history; // Composition
 
-    public Member(String name, String omang) {
+    public Members(String name, String omang) {
         this.name = name;
         this.omang = omang;
         this.balance = 0.0;
@@ -25,6 +25,9 @@ public class Member {
     public double getBalance() { return balance; }
     public ArrayList<Transaction> getHistory() { return history; }
 }
+
+
+
 
 
 
